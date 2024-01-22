d2d86f7bea4ac9674f45f4798cf8d22de933322a
tomerm1 <tomer.meidan1@mail.huji.ac.il>
2024-01-22 22:06:50 +0200

    npm

---
+++ beck/section 26/2.2 Native Modules/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+
+// fs.writeFile("message.txt", "Hello from Tomer", (err) => {
+//     if (err) throw err;
+    
+//     console.log("File has been saved");
+// });
+
+fs.readFile("./message.txt", 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);})
---
+++ beck/section 27/2.3 NPM/index.js
@@ -0,0 +1,7 @@
+import  gerenateSuperhero from "superheros"
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log('my name is ' + sillyName);
+var superheroName = gerenateSuperhero();
+console.log(superheroName);
